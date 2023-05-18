f90194b6aedfa8b3cc0b8b36e0e4d3af696b102e
mcskny <132782511+mcskny@users.noreply.github.com>
2023-05-19 01:11:23 +0300

    v18

---
+++ user-interface/user-main-page.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+<body>
+    
+    <div class="bg-white fixed drop-shadow-[0_5px_15px_rgba(29,13,202,0.25)] w-full h-14">
+        <img class="w-16 ml-5 mb-4 fixed" src="/images/HaberUpLogo.png">
+    </div>
+    
+    
+   
+</body>
+</html>
+
